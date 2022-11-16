223c9e2d3558d2faf88b344d7e3d09e9425d2a0f
unknown <fff8965@gmail.com>
2022-11-17 05:54:57 +0900

    c

--- gulpfile.js
+++ gulpfile.js
@@ -1,25 +1,35 @@
 const { src, dest, series } = require('gulp');
 const closureCompiler = require('google-closure-compiler').gulp();
 var exec = require('child_process').exec;
+const wrapper = require('gulp-wrapper');
 
 
 function min() {
   return src('./source/source.js', {base: './'})
         .pipe(closureCompiler({
             compilation_level: 'SIMPLE',
-            warning_level: 'VERBOSE',
+            warning_level: 'DEFAULT',
             language_in: 'ECMASCRIPT6_STRICT',
             language_out: 'ECMASCRIPT5_STRICT',
-            output_wrapper: '(function(){\n%output%\n}).call(this)',
+            // output_wrapper: '(function(){\n%output%\n}).call(this)',
             js_output_file: 'output.min.js'
             }, {
             platform: ['native', 'java', 'javascript']
             }))
         .pipe(dest('./dist/js'));
+  // https://github.com/google/closure-compiler
+}
+
+function addWrapper(){
+    return src('source/source.js')
+        .pipe(wrapper({
+            header: '(function(){\n',
+            footer: '}).call(this)\n'
+        }))
+        .pipe(dest('source'));
 }
 
 function tsc(cb){
-    
     exec('npx tsc -p tsconfig.json -out ./source/source.js', (error, stdout, stderr) => {
         if (error) {
           console.error(`exec error: ${error}`);
@@ -32,4 +42,6 @@ function tsc(cb){
 
 
 exports.tsc = tsc;
-exports.default = series(tsc, min);
+exports.min = min;
+exports.addWrapper = addWrapper;
+exports.default = series(tsc, addWrapper, min);
